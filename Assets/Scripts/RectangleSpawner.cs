f0622d1184e5752a1e80d79491639a2c593a661e
kricto <shaperd@mail.ru>
2019-10-10 21:37:54 +0300

    First project commit

---
+++ Assets/Scripts/RectangleSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectangleSpawner : MonoBehaviour
+{
+    private static RectangleSpawner instance;
+    public static RectangleSpawner Instance
+    {
+        get => instance;
+
+        set
+        {
+            if(instance == null)
+            {
+                instance = value;
+            }
+            else
+            {
+                Destroy(value.gameObject);
+            }
+        }
+    }
+
+    public bool aboveRectangle = false;
+
+    [SerializeField] private GameObject rectanglePrefab;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(!aboveRectangle)
+                SpawnRectangle();
+        }
+    }
+
+    private void SpawnRectangle()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+
+        if (Physics2D.OverlapBox(v3, new Vector2(8, 4), 0) == null)
+        {
+
+            GameObject obj = Instantiate(rectanglePrefab);
+
+            obj.transform.position = v3;
+
+            obj.GetComponent<RectangleUnit>().ChangeColor();
+        }
+    }
+}
---
+++ Assets/Scripts/RectangleUnit.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RectangleUnit : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private SpriteRenderer myRenderer;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        RectangleSpawner.Instance.aboveRectangle = true;
+    }
+
+    private void OnMouseExit()
+    {
+        RectangleSpawner.Instance.aboveRectangle = false;
+    }
+
+    public void ChangeColor()
+    {
+        myRenderer.color = new Color32((byte)Random.Range(0, 255),
+                                       (byte)Random.Range(0, 255),
+                                       (byte)Random.Range(0, 255),
+                                       255);
+    }
+
+    public void CreateLink()
+    {
+        
+    }
+
+    public void DestroyLink()
+    {
+
+    }
+}
